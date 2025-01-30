12886e332f62c710ceafe201074b64ae93a42b26
k022c0054 <k022c0054@g.neec.ac.jp>
2025-01-30 06:16:21 +0000

    とりあえず

--- php/app/private/controller/terms.php
+++ php/app/private/controller/terms.php
@@ -1,3 +1,8 @@
 <?php
 
-echo '利用規約ページです。';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../view');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('TermsView.html.twig');
